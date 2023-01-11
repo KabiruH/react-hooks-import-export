8394eec7e4a92991fa5a720e47686b8341b383b2
King <kelkab46@gmail.com>
2023-01-11 14:24:40 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees, wildlife} from "./parks/RockyMountain"
 import ColoradoStateParks from "./ColoradoStateParks";
 
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+ReactDOM.render(<MesaVerde/>, document.getElementById("root"));
+
+
